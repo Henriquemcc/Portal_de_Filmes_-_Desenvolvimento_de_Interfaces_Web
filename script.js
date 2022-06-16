8371e0204e7f7a1afc6be1e4d48ae2b73bd7e7af
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-16 16:27:18 -0300

    - Alterando a forma como é contruída a mensagem de erro. - Removendo quebra de linhas da 'htmlString' de 'Lançamentos'

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ function construirFilmesLancamentos() {
 
   // Em caso de erro na execução da requisição, a página será construida para exibir uma mensagem de erro.
   xmlHttpRequestObject.onerror = function () {
-    document.body.innerHTML = `
-<main>
-    <div style="text-align: center;">
-        <h1>Erro ao construir página HTML</h1>
-        <h2>Erro ao realizar 'XMLHttpRequest'</h2>
-        <p style="color: red;">Status: ${this.status}</p>
-        <p style="color: red;">Status Text:${this.statusText}</p>
-        <p style="color: red;">Response Text:${this.responseText}</p>
-    </div>
-</main>
-`;
+    let htmlString = "";
+    htmlString += "<main>";
+    htmlString += '<div style="text-align: center;">';
+    htmlString += "<h1>Erro ao construir página HTML</h1>";
+    htmlString += "<h2>Erro ao realizar 'XMLHttpRequest'</h2>";
+    htmlString += `<p style="color: red;">Status: ${this.status}</p>`;
+    htmlString += `<p style="color: red;">Status Text:${this.statusText}</p>`;
+    htmlString +=
+      `<p style="color: red;">Response Text:${this.responseText}</p>`;
+    htmlString += "</div>";
+    htmlString += "</main>";
+
+    document.body.innerHTML = htmlString;
   };
 
   // Em caso de sucesso na execução da requisição, a página será construida com os dados obtidos por esta função.
@@ -38,25 +40,25 @@ function construirFilmesLancamentos() {
       dados.results.forEach(
         (value, index) => {
           if (index == 0) {
-            htmlString += `<div class="carousel-item active">\n`; // div1: Inicio
+            htmlString += `<div class="carousel-item active">`; // div1: Inicio
           } else {
-            htmlString += `<div class="carousel-item">\n`; // div1: Inicio
+            htmlString += `<div class="carousel-item">`; // div1: Inicio
           }
-          htmlString += '<div class="row">\n'; // div2: Inicio
-          htmlString += '<div class="col-6">\n'; // div3: Inicio
-          htmlString += `<h2>${value.title}</h2>\n`;
-          htmlString += `<p>${value.overview}</p>\n`;
-          htmlString += "</div>\n"; // div3: Fim
-          htmlString += '<div class="col-6">\n'; // div4: Inicio
+          htmlString += '<div class="row">'; // div2: Inicio
+          htmlString += '<div class="col-6">'; // div3: Inicio
+          htmlString += `<h2>${value.title}</h2>`;
+          htmlString += `<p>${value.overview}</p>`;
+          htmlString += "</div>"; // div3: Fim
+          htmlString += '<div class="col-6">'; // div4: Inicio
 
           if (value.video) {}
           else {
             htmlString +=
-              `<img class="movie-poster" src="https://image.tmdb.org/t/p/original/${value.poster_path}">\n`;
+              `<img class="movie-poster" src="https://image.tmdb.org/t/p/original/${value.poster_path}">`;
           }
-          htmlString += "</div>\n"; // div4: Fim
-          htmlString += "</div>\n"; // div2: Fim
-          htmlString += "</div>\n"; // div1: Fim
+          htmlString += "</div>"; // div4: Fim
+          htmlString += "</div>"; // div2: Fim
+          htmlString += "</div>"; // div1: Fim
         },
       );
     }
