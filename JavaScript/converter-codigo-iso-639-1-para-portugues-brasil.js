3f3145a830d86e5be65ccb56ae4331763bf265ee
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-19 21:46:42 -0300

    Adicionando função 'converterCodigoPaisIso6391ParaPortuguesBrasil' e renomeando função 'converterCodigoIso6391ParaPortuguesBrasil' para 'converterCodigoLinguaIso6391ParaPortuguesBrasil'

--- JavaScript/converter-codigo-iso-639-1-para-portugues-brasil.js
+++ JavaScript/converter-codigo-iso-639-1-para-portugues-brasil.js
@@ -1,11 +1,24 @@
 /**
- * Converte cógido em ISO 639-1 para Português Brasil.
+ * Converte código de um idioma em ISO 639-1 para Português Brasil.
  * @param {String} codigo Código ISO 639-1.
  * @returns Valor em Português do código.
  */
-export function converterCodigoIso6391ParaPortuguesBrasil(codigo) {
+export function converterCodigoLinguaIso6391ParaPortuguesBrasil(codigo) {
   let nomeLinguasEmPortugues = new Intl.DisplayNames(["pt-BR"], {
     type: "language",
+  });
+
+  return nomeLinguasEmPortugues.of(codigo);
+}
+
+/**
+ * Converte código de um país em ISO 639-1 para Português Brasil.
+ * @param {String} codigo Código ISO 639-1.
+ * @returns Valor em Português do código.
+ */
+export function converterCodigoPaisIso6391ParaPortuguesBrasil(codigo) {
+  let nomeLinguasEmPortugues = new Intl.DisplayNames(["pt-BR"], {
+    type: "region",
   });
 
   return nomeLinguasEmPortugues.of(codigo);
