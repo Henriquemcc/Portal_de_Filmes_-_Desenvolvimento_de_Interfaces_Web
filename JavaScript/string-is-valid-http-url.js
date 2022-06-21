43c675a85aa4cc31ed87ac2e68874bdbcd51f2aa
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-21 18:34:20 -0300

    - Adicionando impressão da mensagem de erro no bloco catch de try catch. - Corrigindo documentação. - Re-indentando o código fonte.

--- JavaScript/string-is-valid-http-url.js
+++ JavaScript/string-is-valid-http-url.js
@@ -1,13 +1,19 @@
 /**
  * Verifica se uma string é uma URL http válida.
  * @param {String} str String a ser verificada.
- * @return Valor booleano indicando se a String passada é uma URL http válida.
+ * @return {Boolean} Valor booleano indicando se a String passada é uma URL http válida.
  */
-export function stringIsValidHttpUrl(str) {
-  try {
-    let url = new URL(str);
-    return url.protocol === "http:" || url.protocol === "https:";
-  } catch {
-    return false;
-  }
+export function stringIsValidHttpUrl(str)
+{
+	try
+	{
+		let url = new URL(str);
+		return url.protocol === "http:" || url.protocol === "https:";
+	}
+	catch (e)
+	{
+		console.log(e);
+	}
+
+	return false;
 }
