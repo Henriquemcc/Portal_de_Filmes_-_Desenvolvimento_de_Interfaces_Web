100080b02b4f5202ab852c791bbbd1828680e5b0
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-21 19:30:25 -0300

    - Reduzindo o uso de variáveis globais. - Adicionando checagem para dados nulos na hora de construir a página HTML. - Alterando como são passados os parâmetros de queryString para a url da requisição web. - Alterando como a função 'construirPaginaErroXmlHttpRequest' é chamada. - Corrigindo nome de funções. - Corrigindo documentação. - Re-indentando o código fonte.

--- pesquisar/script.js
+++ pesquisar/script.js
@@ -1,110 +1,146 @@
-import { MOVIE_DB_API_KEY, LANGUAGE } from "../JavaScript/constantes.js";
-import { construirPaginaErroXmlHttpRequest } from "../JavaScript/construir-pagina-erro-xml-http-request.js";
-import { converterUrlImagemTheMovieDb } from "../JavaScript/converter-url-imagem-the-movie-db.js";
-
-/**
- * Armazena os dados obtidos pela pesquisa.
- */
-let dadosObtidosPelaPesquisa;
+import {LANGUAGE, MOVIE_DB_API_KEY} from "../JavaScript/constantes.js";
+import {construirPaginaErroXmlHttpRequest} from "../JavaScript/construir-pagina-erro-xml-http-request.js";
+import {converterUrlImagemTheMovieDb} from "../JavaScript/converter-url-imagem-the-movie-db.js";
 
 /**
  * Obtém do 'The Movie DB' os dados encontrados com a(s) palavra(s) pesquisada(s).
  * @param {String} query Palavra(s) pesquisada(s).
+ * @param {String} api_key Chave de API do 'The Movie DB'.
+ * @param {String} language Linguagem a ser mostrada o resultado.
+ * @returns {Object} Objeto contendo os dados da pesquisa.
  */
-function ObterDadosDaPesquisa(query) {
-  let xmlHttpRequestObject = new XMLHttpRequest();
-  xmlHttpRequestObject.open(
-    "GET",
-    `https://api.themoviedb.org/3/search/multi?api_key=${MOVIE_DB_API_KEY}&language=${LANGUAGE}&query=${query}`,
-    false,
-  );
-  xmlHttpRequestObject.onerror = construirPaginaErroXmlHttpRequest;
-  xmlHttpRequestObject.send();
-  dadosObtidosPelaPesquisa = JSON.parse(xmlHttpRequestObject.responseText);
+function obterDadosDaPesquisa(query, api_key, language)
+{
+    let url = new URL('https://api.themoviedb.org/3/search/multi');
+    url.searchParams.set("api_key", api_key);
+    url.searchParams.set("language", language);
+    url.searchParams.set("query", query);
+
+    let xmlHttpRequestObject;
+    try
+    {
+        xmlHttpRequestObject = new XMLHttpRequest();
+        xmlHttpRequestObject.open("GET", url, false);
+        xmlHttpRequestObject.send();
+        return JSON.parse(xmlHttpRequestObject.responseText);
+    }
+    catch (e) {
+        construirPaginaErroXmlHttpRequest({xmlHttpRequestObject: xmlHttpRequestObject});
+        console.log(e);
+    }
+
+    return null;
 }
 
 /**
  * Obtém do navegador do usuário o valor do parâmetro 'search'.
- * @returns Valor do parâmetro 'search'.
+ * @returns {String} Valor do parâmetro 'search'.
  */
-function ObterQueryStringPesquisa() {
-  let url = new URL(window.location.href);
-  return url.searchParams.get("search");
+function obterQueryStringPesquisa()
+{
+	let url = new URL(window.location.href);
+	return url.searchParams.get("search");
 }
 
-function ConstruirPaginaComDadosDaPesquisa() {
-  let htmlString = "";
+/**
+ * Constrói a página com os dados da pesquisa.
+ * @param {Object} dados Dados obtidos do 'The Movie DB' sobre a pesquisa.
+ */
+function construirPaginaComDadosDaPesquisa(dados)
+{
+	let htmlString = "";
 
-  dadosObtidosPelaPesquisa.results.forEach(
-    (resultadoPesquisa) => {
-      htmlString += '<div class="row">';
-      htmlString += '<div class="box-resultado">';
+	dados.results.forEach(
+		(resultadoPesquisa) =>
+		{
+			htmlString += '<div class="row">';
+			htmlString += '<div class="box-resultado">';
 
-      let tipoConteudo;
-      let titulo;
-      let tituloOriginal;
-      let descricao;
-      switch (resultadoPesquisa.media_type) {
-        case "movie":
-          tipoConteudo = "Filme";
-          titulo = resultadoPesquisa.title;
-          tituloOriginal = resultadoPesquisa.original_title;
-          descricao = resultadoPesquisa.overview;
-          break;
-        case "tv":
-          tipoConteudo = "Programa de TV";
-          titulo = resultadoPesquisa.name;
-          tituloOriginal = resultadoPesquisa.original_name;
-          descricao = resultadoPesquisa.overview;
-          break;
-      }
+			let tipoConteudo;
+			let titulo;
+			let tituloOriginal;
+			let descricao;
+			switch (resultadoPesquisa.media_type)
+			{
+				case "movie":
+					tipoConteudo = "Filme";
+					titulo = resultadoPesquisa.title;
+					tituloOriginal = resultadoPesquisa.original_title;
+					descricao = resultadoPesquisa.overview;
+					break;
+				case "tv":
+					tipoConteudo = "Programa de TV";
+					titulo = resultadoPesquisa.name;
+					tituloOriginal = resultadoPesquisa.original_name;
+					descricao = resultadoPesquisa.overview;
+					break;
+			}
 
-      // Exibindo a imagem
-      let urlImagem = converterUrlImagemTheMovieDb(
-        resultadoPesquisa.poster_path,
-      );
-      if (urlImagem != null) {
-        htmlString += `<img src="${urlImagem}" alt="${titulo}">`;
-      }
+			// Exibindo a imagem
+			let urlImagem = converterUrlImagemTheMovieDb(resultadoPesquisa.poster_path);
+			if (urlImagem != null)
+			{
+				htmlString += `<img src="${urlImagem}" alt="${titulo}">`;
+			}
 
-      // Exibindo o tipo de conteúdo
-      htmlString += `<span class="tipo-conteudo">${tipoConteudo}</span><br>`;
+			// Exibindo o tipo de conteúdo
+			if (tipoConteudo != null)
+			{
+				htmlString += `<span class="tipo-conteudo">${tipoConteudo}</span><br>`;
+			}
 
-      // Exibindo o titulo
-      htmlString +=
-        `<span class="titulo-original">${tituloOriginal}</span><br>`;
+			// Exibindo o titulo original
+			if (tituloOriginal != null)
+			{
+				htmlString += `<span class="titulo-original">${tituloOriginal}</span><br>`;
+			}
 
-      // Exibindo o
-      htmlString += `<span class="titulo">${titulo}</span><br>`;
-      htmlString += `<span class="descricao">${descricao}</span><br>`;
+			// Exibindo o título
+			if (titulo != null)
+			{
+				htmlString += `<span class="titulo">${titulo}</span><br>`;
+			}
 
-      let url;
-      if (resultadoPesquisa.media_type === "tv") {
-        url = `../programa-de-tv/?id=${resultadoPesquisa.id}`;
-      } else if (resultadoPesquisa.media_type === "movie") {
-        url = `../filme/?id=${resultadoPesquisa.id}`;
-      }
+			// Exibindo a descrição
+			if (descricao != null)
+			{
+				htmlString += `<span class="descricao">${descricao}</span><br>`;
+			}
 
-      htmlString +=
-        `<a href="${url}"><span class="mais-informacoes">Mais informações</a>`;
-      htmlString += "</div>";
-      htmlString += "</div>";
-    },
-  );
+			// Adicionando o link de mais informações
+			if (resultadoPesquisa.media_type != null && resultadoPesquisa.id != null)
+			{
+				let url;
+				if (resultadoPesquisa.media_type === "tv")
+				{
+					url = `../programa-de-tv/?id=${resultadoPesquisa.id}`;
+				}
+				else if (resultadoPesquisa.media_type === "movie")
+				{
+					url = `../filme/?id=${resultadoPesquisa.id}`;
+				}
 
-  document.querySelector("#section_resultado_pesquisa").innerHTML = htmlString;
+				htmlString += `<a href="${url}"><span class="mais-informacoes">Mais informações</a>`;
+			}
+			htmlString += "</div>";
+			htmlString += "</div>";
+		},
+	);
+
+	document.querySelector("#section_resultado_pesquisa").innerHTML = htmlString;
 }
 
-onload = () => {
-  // Obtendo a queryString 'search' da página atual
-  let pesquisa = ObterQueryStringPesquisa();
+onload = () =>
+{
+	// Obtendo a queryString 'search' da página atual
+	let pesquisa = obterQueryStringPesquisa();
 
-  // Adicionando a queryString 'search' á barra de pesquisa
-  document.querySelector("#search-bar").value = pesquisa;
+	// Adicionando a queryString 'search' á barra de pesquisa
+	document.querySelector("#search-bar").value = pesquisa;
 
-  // Obtendo os dados da pesquisa
-  ObterDadosDaPesquisa(pesquisa);
+	// Obtendo os dados da pesquisa
+	let dadosDaPesquisa = obterDadosDaPesquisa(pesquisa, MOVIE_DB_API_KEY, LANGUAGE);
 
-  // Construindo a página
-  ConstruirPaginaComDadosDaPesquisa();
+	// Construindo a página
+	construirPaginaComDadosDaPesquisa(dadosDaPesquisa);
 };
